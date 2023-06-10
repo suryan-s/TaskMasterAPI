cc75ef0880ffeb6d40fb7093ec60703b88944020
Suryan S <suryannasa@gmail.com>
2023-06-10 13:31:37 +0530

    UPDATED files for deployment

--- TaskMasterAPI/settings.py
+++ TaskMasterAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import rest_framework_simplejwt
 from datetime import timedelta
@@ -22,12 +23,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--f3xbe$a*+d2@b7p8($h#_qzu2#&z1(6kn=%yi+mzy=k*$jfo5'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='i-know-you-wanna-look-into-this-but-sorry-you-cant')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
